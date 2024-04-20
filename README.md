3d147f5fd164f32f7e5731ec04fbaddfade738ef
VypperFO <felixlatulip@gmail.com>
2024-04-20 15:36:08 -0400

    Image resizer done

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# image-processor
+# Image Processor for Universe Boardshop
+
+## Step 1
+`npm install`
+## Step 2
+`docker-compose up --build`
---
+++ server.ts
@@ -0,0 +1,54 @@
+import express, {Express, Request, Response} from "express";
+import bodyParser from "body-parser";
+import multer from "multer";
+import sharp from "sharp";
+import cors from "cors";
+
+const app: Express = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use(express.static("public"));
+
+const storage = multer.memoryStorage();
+const upload = multer({storage: storage});
+
+app.post(
+  "/api/resize",
+  upload.single("image"),
+	async function (req: Request, res: Response) {
+    if (req.file) {
+      const filename =
+        new Date().toISOString().replace(/:/g, "-") +
+        "-" +
+        req.file.originalname;
+
+			try {
+        const resizedImageBuffer = await sharp(req.file.buffer)
+          .resize({
+            width: 1728,
+            height: 2160,
+            fit: sharp.fit.contain,
+            background: {r: 255, g: 255, b: 255, alpha: 1},
+          })
+          .png()
+          .toBuffer();
+
+        res.type("png");
+
+        res.send(resizedImageBuffer);
+      } catch (error: any) {
+        res.status(500).json({
+          message: "Failed to process image",
+          error: error.message,
+        });
+      }
+    } else {
+      res.status(400).json({message: "No file uploaded"});
+    }
+  }
+);
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
